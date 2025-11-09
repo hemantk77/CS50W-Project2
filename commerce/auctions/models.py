b342eeab89fb8e282f6b70d642f1386c6887b29c
hemantk77 <hemant11uk@gmail.com>
2025-11-10 00:57:55 +0100

    AuctionListing model created in models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    is_active = models.BooleanField(default=True)
+    image = models.URLField(blank=True, null=True)
