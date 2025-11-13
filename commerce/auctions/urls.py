f886f20a01b6f7accaa0fad06c9ddcf7e1c7f933
hemantk77 <hemant11uk@gmail.com>
2025-11-13 09:53:44 +0100

    New url path "won_auctions/" in urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("listing/<int:listing_id>", views.listing_page, name="listing_page"),
     path("watchlist/<int:listing_id>", views.watchlist, name="watchlist"),
     path("close/<int:listing_id>", views.close_auction, name="close_auction"),
+    path("won_auctions/", views.won_auctions, name="won_auctions"),
 ]
