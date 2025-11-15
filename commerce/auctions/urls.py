29b0c0db5d5a2ad657a6ab2dec9acd7727cb2da1
hemantk77 <hemant11uk@gmail.com>
2025-11-15 12:44:14 +0100

    Added a new path "watchlist/" in urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("watchlist_toggle/<int:listing_id>", views.watchlist_toggle, name="watchlist_toggle"),
     path("close/<int:listing_id>", views.close_auction, name="close_auction"),
     path("won_auctions/", views.won_auctions, name="won_auctions"),
+    path("watchlist/", views.watchlist, name="watchlist"),
 ]
