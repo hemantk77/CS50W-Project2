f44abc683b221f4f935a1dcb2bee5762ab00746c
hemantk77 <hemant11uk@gmail.com>
2025-11-16 23:58:19 +0100

    Updated the view "categories_list"

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -222,7 +222,7 @@ def watchlist(request):
     })
 
 def categories_list(request):
-    categories = AuctionListing.objects.filter(is_active=True).values("category").annotate(count=Count("category")).order_by("category")
+    categories = AuctionListing.objects.filter(is_active=True).exclude(category="").values("category").annotate(count=Count("category")).order_by("category")
     
     return render(request, "auctions/categories_list.html", {
         "categories":categories
